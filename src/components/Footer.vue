83d5bd408e7f6d6812ca8babd36222a9f2cc844a
HOU Zhenni <hzn2017@outlook.com>
2021-05-09 01:29:48 +0800

    feat:migrate zoom pic func

---
+++ src/components/Footer.vue
@@ -0,0 +1,102 @@
+<template>
+  <footer>
+    <p>Created with Vite.js & Vue.js <br>
+    	Designed by  
+    	<a href="https://www.houzhenni.com" target="_blank" class="no-decor-link">
+    		<strong>Jenny HOU</strong>
+	    	<i class="fa fa-github font-20"></i>
+	    </a>
+    </p>
+    <div class="go-top" @click="toTop" :class="{'go-top-hidden': isHidden}">
+        <div class="arrow"></div>
+        <div class="stick"></div>
+    </div>
+  </footer>
+</template>
+
+<script type="text/javascript">
+export default {
+	data() {
+		return {
+			isHidden: true
+		}
+	},
+	methods: {
+		toTop: function() {
+			document.querySelector(".desk-wrap").scrollIntoView({behavior: 'smooth'})
+		}
+	},
+	mounted: function() {
+		var that = this;
+		window.addEventListener("scroll", function(){
+			if (window.scrollY > 199) {
+				that.isHidden = false;
+			} else {
+				that.isHidden = true;
+			}
+		});
+	}
+}
+</script>
+
+<style type="text/css">
+.go-top {
+    opacity: 0.6;
+    position: fixed;
+    bottom: 40px;
+    right: 20px;
+    border: 1px solid gray;
+    width: 38px;
+    height: 38px;
+    background-color: rgba(249, 249, 249, 1);
+    border-radius: 3px;
+    cursor: pointer;
+    transition: opacity .3s;
+}
+.go-top:hover {
+    opacity: 1;
+}
+.go-top .arrow {
+    position: absolute;
+    left: 10px;
+    top: -1px;
+    width: 0;
+    height: 0;
+    border: 9px solid transparent;
+    border-bottom-color: gray;
+}
+.go-top .stick {
+    position: absolute;
+    left: 15px;
+    top: 15px;
+    width: 8px;
+    height: 14px;
+    display: block;
+    background-color: gray;
+    -webkit-border-radius: 1px;
+    -moz-border-radius: 1px;
+    border-radius: 1px;
+}
+/* toTop button end */
+.go-top-hidden {
+	opacity: 0;
+	display: none;
+}
+footer {
+	padding: 20px;
+	color: white;
+	background: hsla(0, 0%, 20%, 1);
+}
+footer p {
+	margin: 0 auto;
+	font-size: 14px;
+	text-align: center;
+}
+footer a {
+	color: white;
+	text-decoration: none;
+}
+.hidden {
+	display: none;
+}
+</style>
--- src/desk.vue
+++ src/desk.vue
@@ -1,40 +1,44 @@
 <template>
-  <div class="desk-wrap">
-    <main class="desk-main">
-      <div class="banner main-color">
-        <div class="upper">
-          <span class="left"></span>
-          <span>&nbsp;&nbsp;FROM THE DESK OF&nbsp;&nbsp;</span>
-          <span class="right"></span>
+  <div>
+    <div class="desk-wrap">
+      <main class="desk-main">
+        <div class="banner main-color">
+          <div class="upper">
+            <span class="left"></span>
+            <span>&nbsp;&nbsp;FROM THE DESK OF&nbsp;&nbsp;</span>
+            <span class="right"></span>
+          </div>
+          <div class="lower">
+            ZHENNI J. HOU
+          </div>
         </div>
-        <div class="lower">
-          ZHENNI J. HOU
+        <DeskPost 
+        url="https://www.houzhenni.com/desk"
+        author="Zhenni J. HOU" 
+        v-for="(post) in posts" 
+        :key="post.id"
+        :id="post.id" :time="post.time" :content="post.content"
+        :picture="post.picture"
+        ></DeskPost>
+      </main>
+      <aside class="desk-side">
+        <div class="main-color" id="my-info">
+          <div class="silver-bg"></div>
+          <div class="pic">
+            <img src="https://www.houzhenni.com/img/mypic.png">
+          </div>
+          <div class="intro">
+            自学前端开发，专业电商运营，熟练掌握 PS 和 Excel 的 72 种用法，欢迎来我的小站玩耍~
+          </div>
         </div>
-      </div>
-      <DeskPost 
-      url="https://www.houzhenni.com/desk"
-      author="Zhenni J. HOU" 
-      v-for="(post) in posts" 
-      :key="post.id"
-      :id="post.id" :time="post.time" :content="post.content"
-      :picture="post.picture"
-      ></DeskPost>
-    </main>
-    <aside class="desk-side">
-      <div class="main-color" id="my-info">
-        <div class="silver-bg"></div>
-        <div class="pic">
-          <img src="https://www.houzhenni.com/img/mypic.png">
+        <div class="support main-color-bg">
+          <a target="_blank" href="https://afdian.net/@c53hzn">支持我</a>
         </div>
-        <div class="intro">
-          自学前端开发，专业电商运营，熟练掌握 PS 和 Excel 的 72 种用法，欢迎来我的小站玩耍~
-        </div>
-      </div>
-      <div class="support main-color-bg">
-        <a target="_blank" href="https://afdian.net/@c53hzn">支持我</a>
-      </div>
-    </aside>
-  </div>    
+      </aside>
+    </div>  
+    <Footer></Footer>
+  </div>
+    
 </template>
 
 <script>
@@ -42,6 +46,7 @@ import getDesk from './getdesk';
 import config from './desk.config';
 import yaml from "yaml";
 import DeskPost from "./components/Desk-Post.vue";
+import Footer from "./components/Footer.vue"
 
 export default {
 	name: "desk",
@@ -52,9 +57,42 @@ export default {
   },
 	mounted() {
 		this.loadPosts();
+    var d = document;
+    var maskStyle = d.createElement("style");
+    var maskDom = d.createElement("div");
+    maskDom.id = "mask_layer";
+    maskDom.className = "hidden";
+    maskDom.innerHTML = `<div id="mask_child"></div>`;
+    d.querySelector("head").appendChild(maskStyle);
+    d.querySelector("body").appendChild(maskDom);
+    d.querySelector("#mask_child").onclick = function() {
+      d.querySelector("#mask_layer").className = "fade-out-anime";
+    }
+    function zoom(selector,isImg) {
+      var nodes = d.querySelectorAll(selector);
+      for (let i = 0; i < nodes.length; i++) {
+        let parent = nodes[i].parentNode;
+        let grandParent = parent.parentNode;
+        if (parent.nodeName != "A" && grandParent.nodeName != "A") {
+          nodes[i].className = nodes[i].className + " zoomable";
+          nodes[i].onclick = function() {
+            if (isImg) {
+              d.querySelector("#mask_child").style.backgroundImage = "url("+this.src+")";
+            } else {
+              d.querySelector("#mask_child").style.backgroundImage = this.style.backgroundImage;
+            }
+            d.querySelector("#mask_layer").className = "fade-in-anime";
+          }
+        }
+      }
+    }
+    setTimeout(function(){
+      zoom(".img img",true);
+    },1000)
 	},
 	components: {
-    DeskPost
+    DeskPost,
+    Footer
 	},
   methods: {
   	loadPosts() {
@@ -70,6 +108,9 @@ export default {
 <style type="text/css">
 * {
 	box-sizing: border-box;
+}
+html, body {
+  margin: 0px;
 }
 .main-color {
   color: #182b54;
@@ -209,6 +250,94 @@ export default {
   }
   #my-info .pic {
     margin: 0px auto;
+  }
+}
+
+.zoomable {
+  cursor: zoom-in;
+}
+#mask_layer {
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, .5);
+}
+#mask_child {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  margin: auto;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  cursor: zoom-out;
+}
+#mask_layer .arrow {
+  position: absolute;
+  top: 50%;
+  left: 0px;
+  right: 0px;
+  width: 48px;
+  font-size: 24px;
+  color: white;
+  text-align: center;
+  line-height: 50px;
+  vertical-align: top;
+  background: rgba(0,0,0,0.5);
+  transform: translateY(-25px);
+  user-select: none;
+  cursor: pointer;
+}
+#mask_layer .arrow:hover {
+  opacity: 0.7;
+}
+#mask_layer .arrow-left {
+  left: 0px;
+  right: auto;
+}
+#mask_layer .arrow-right {
+  left: auto;
+  right: 0px;
+}
+.hidden {
+  display: none;
+}
+.fade-in-anime {
+  animation: fade-in 0.5s;
+    z-index: 2;
+}
+.fade-out-anime {
+  animation: fade-out 0.5s;
+  animation-fill-mode: forwards;
+}
+@keyframes fade-in {
+  from {
+    opacity: 0;
+    z-index: 2;
+  }
+  to {
+    opacity: 1;
+    z-index: 2;
+  }
+}
+@keyframes fade-out {
+  0% {
+    opacity: 1;
+    z-index: 2;
+  }
+  99% {
+    opacity: 0;
+    z-index: 2;
+  }
+  100% {
+    opacity: 0;
+    z-index: -1;
   }
 }
 </style>
